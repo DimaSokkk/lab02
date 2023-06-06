345da1a7ab9aa44491dda13c90831687ed129fdb
DimaSokkk <dimabomonka@mail.ru>
2023-06-06 20:11:32 +0300

    Update code

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+   
 int main(int argc, char** argv){
   string name;                                           // Имя пользователя
   std::cin >> name;                                      // Ввод имени пользователя
