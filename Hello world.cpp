a95b6d6896499d357e8eae942b728b57a6f1e3bc
DimaSokkk <dimabomonka@mail.ru>
2023-06-06 20:10:14 +0300

    Fixed cpp file

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
 
-int
-main(int argc, char** argv)
-{
-  string name;                                           // Name of @user
-  std::cin >> name;                                      // Input name of @user
-  std::cout << "Hello world from " << name << std::endl; // Output name of @user
-}
+int main(int argc, char** argv){
+  string name;                                           // Имя пользователя
+  std::cin >> name;                                      // Ввод имени пользователя
+  std::cout << "Hello world from " << name << std::endl; // Вывод данных
+} 
